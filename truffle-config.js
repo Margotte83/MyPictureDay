51e3e14b0d0e1e5ef39a602e6a6d99706499b755
marjorie <marjorie.ngoupende@gmail.com>
2022-01-31 17:50:13 +0100

    Fix RPC Network

--- truffle-config.js
+++ truffle-config.js
@@ -7,5 +7,14 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    matic: {
+      networkCheckTimeout: 10000, 
+      provider: () => new HDWalletProvider(mnemonic, `https://matic-mumbai.chainstacklabs.com`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      
+    },
   }
 }
